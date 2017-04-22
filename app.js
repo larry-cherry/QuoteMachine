acc87b551020bbc747be2e01de53285b4e6ae596
Larry D. Cherry <larry.cherry.0406@gmail.com>
2017-04-22 14:27:29 -0500

    animated text when button is pressed

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
   console.log("app.js loaded with jquery")
+  $('#newquote').on("click", function(e) {
+    document.getElementById("newquote").disabled = true;
+    $('#quote-container').children().addClass("animated fadeOut");
+    removeQuote = function() {
+      $('#quote-container').children().remove();
+      document.getElementById("quote-container").innerHTML += '<p class="animated fadeIn">Hello World!</p>'
+      setTimeout(enableButton, 100)
+    }
+    setTimeout(removeQuote, 2000)
+    enableButton = function() {
+      document.getElementById("newquote").disabled = false;
+    }
+  })
 });
