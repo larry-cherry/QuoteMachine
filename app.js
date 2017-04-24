afaad86d17fc30ebfbe99f50fe9529abd213a8b0
larry-cherry <larry.cherry.0406@gmail.com>
2017-04-24 16:10:47 -0500

    cleaned up the AJAX code some

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
 $(document).ready(function(){
   console.log("app.js loaded with jquery")
+  var enableButton = function() {
+    document.getElementById("newquote").disabled = false;
+  }
+
+  var changeQuote = function(server_data) {
+    // debugger;
+    var es6Quote = `<div class="animated fadeIn text-primary"> ${server_data[0]["content"]}
+     <br><h3 class="pull-right">${server_data[0]["title"]}</h3></div>`;
+    document.getElementById("quote-container").innerHTML = es6Quote;
+    setTimeout(enableButton, 100)
+  }
+
   var getQuote = function() {
     document.getElementById("newquote").disabled = true;
-    var quote = $.ajax({
+    $('#quote-container').children().addClass("animated fadeOut");
+    $.ajax({
       url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
       type: 'GET',
       cache: false,
     }).done(function(server_data){
       console.log("success" + server_data);
-      return server_data;
+      changeQuote(server_data);
       // debugger;
     }).fail(function(jqXHR, textStatus, errorThrown){
       console.log("fail" + errorThrown);
       return "There was an issue with your request please try again.";;
     });
-    $('#quote-container').children().addClass("animated fadeOut");
-    removeQuote = function() {
-      $('#quote-container').children().remove();
-      var es6Quote = `<div class="animated fadeIn text-primary"> ${quote["responseJSON"][0]["content"]}
-       <br><h3 class="pull-right">${quote["responseJSON"][0]["title"]}</h3></div>`;
-      document.getElementById("quote-container").innerHTML += es6Quote;
-      // document.getElementById("quote-container").innerHTML += '<div class="animated fadeIn text-primary">' + quote["responseJSON"][0]["content"] + '<br>' + '<h3 class="pull-right">' + quote["responseJSON"][0]["title"] + '</h3></div>'
-      setTimeout(enableButton, 100)
-    }
-    setTimeout(removeQuote, 2000)
-    enableButton = function() {
-      document.getElementById("newquote").disabled = false;
-    }
   }
+
   getQuote();
 
   $('#newquote').on("click", function(e) {
